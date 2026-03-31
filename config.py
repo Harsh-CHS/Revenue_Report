2628709e12d5495884e295326a3a79b0178fa550
Harsh-CHS <hghadiya@centrichcare.com>
2026-03-31 12:05:21 -0700

    updated path in config file

--- config.py
+++ config.py
@@ -1,13 +1,24 @@
+# import os
+
+# BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
+# INCOMING_DIR = os.path.join(BASE_DIR, "incoming")
+# OUTPUT_DIR = os.path.join(BASE_DIR, "output")
+# PROCESSED_DIR = os.path.join(BASE_DIR, "processed")
+# ERROR_DIR = os.path.join(BASE_DIR, "error")
+# LOG_DIR = os.path.join(BASE_DIR, "logs")
+# LOG_FILE = os.path.join(LOG_DIR, "automation.log")
+
+# for folder in [INCOMING_DIR, OUTPUT_DIR, PROCESSED_DIR, ERROR_DIR, LOG_DIR]:
+#     os.makedirs(folder, exist_ok=True)
+
 import os
 
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+BASE_DIR = "/data/data"
 
 INCOMING_DIR = os.path.join(BASE_DIR, "incoming")
 OUTPUT_DIR = os.path.join(BASE_DIR, "output")
 PROCESSED_DIR = os.path.join(BASE_DIR, "processed")
-ERROR_DIR = os.path.join(BASE_DIR, "error")
-LOG_DIR = os.path.join(BASE_DIR, "logs")
-LOG_FILE = os.path.join(LOG_DIR, "automation.log")
 
-for folder in [INCOMING_DIR, OUTPUT_DIR, PROCESSED_DIR, ERROR_DIR, LOG_DIR]:
+for folder in [INCOMING_DIR, OUTPUT_DIR, PROCESSED_DIR]:
     os.makedirs(folder, exist_ok=True)
